edce9cfd1c5eff2b20efb0db69b6632c155c085c
srihan93 <srihan93@gmail.com>
2021-06-19 15:38:39 +0530

    removed unused usings

--- GlobalErrorHandling/Controllers/EmployeeController.cs
+++ GlobalErrorHandling/Controllers/EmployeeController.cs
@@ -1,11 +1,8 @@
 ﻿using GlobalErrorHandling.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using GlobalErrorHandling.Exceptions;
-using GlobalErrorHandling.Models;
 using System.Net;
 
 namespace GlobalErrorHandling.Controllers
